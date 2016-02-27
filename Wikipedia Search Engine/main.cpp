795c2073ec01c7c424b65579ad504720f3a68334
LiuzhuoGump <150367735@qq.com>
2016-02-28 00:31:40 +0800

    项目初始化

---
+++ Wikipedia Search Engine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Wikipedia Search Engine
+//
+//  Created by 甘流卓 on 16/2/28.
+//  Copyright © 2016年 甘流卓. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
